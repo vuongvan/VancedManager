46ee388be95c4ea19b9e0998eebe7a82a1bfbc63
Vuong Mai <maivanvuong@gmail.com>
2022-10-27 16:43:57 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.vanced.manager"
         minSdk = 21
         targetSdk = 31
-        versionCode = 221017
+        versionCode = 221027
         versionName = "2.6.5"
 
         vectorDrawables {
